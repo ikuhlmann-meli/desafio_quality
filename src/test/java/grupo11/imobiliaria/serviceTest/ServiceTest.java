4490877db99b0454ccbef11c18a2ba58770cbe92
mbjunqueiraweb <mauro.junqueira@mercadolivre.com>
2022-01-05 17:25:06 -0300

    Tests maiorComodo e area.

--- src/test/java/grupo11/imobiliaria/serviceTest/ServiceTest.java
+++ src/test/java/grupo11/imobiliaria/serviceTest/ServiceTest.java
@@ -1,5 +1,6 @@
 package grupo11.imobiliaria.serviceTest;
 
+import grupo11.imobiliaria.ImobiliariaDTO.ComodoDTO;
 import grupo11.imobiliaria.entity.District;
 import grupo11.imobiliaria.entity.Prop;
 import grupo11.imobiliaria.entity.Room;
@@ -106,4 +107,73 @@ public class ServiceTest {
         //assertion
         assertTrue(p.equals(prop1));
     }
+
+    @Test
+    public void verificaSeMaiorComoFoiDevolvido(){
+        District district1 = new District("Bairro do Limoeiro", new BigDecimal(200));
+
+        List<District> dist = new ArrayList<District>();
+
+        dist.add(district1);
+
+        Room room1 = Room.builder().room_name("cozinha").room_width(3d).room_length(5d).build();
+        Room room2 = Room.builder().room_name("sala").room_width(5d).room_length(6d).build();
+
+        Prop prop1 = Prop.builder()
+                .prop_name("casa1")
+                .prop_district(district1)
+                .prop_rooms(Arrays.asList(room1, room2)).build();
+
+        List<Prop> p = new ArrayList<Prop>();
+        p.add(prop1);
+
+        ImobiliariaRepository mock = Mockito.mock(ImobiliariaRepository.class);
+        Mockito.when(mock.getPropertiesList()).thenReturn(p);
+        ImobiliariaService propService = new ImobiliariaService(mock);
+
+        //act
+        String biggestRoom = propService.maiorComodo(prop1.getProp_name());
+
+        //assert
+        assertEquals(biggestRoom, room2.getRoom_name());
+    }
+
+    @Test
+    public void verificaSeTotalM2Retorna(){
+        District district1 = new District("Bairro do Limoeiro", new BigDecimal(200));
+
+        List<District> dist = new ArrayList<District>();
+        List<ComodoDTO> dto = new ArrayList<ComodoDTO>();
+
+        dist.add(district1);
+
+        Room room1 = Room.builder().room_name("cozinha").room_width(3d).room_length(5d).build();
+        Room room2 = Room.builder().room_name("sala").room_width(5d).room_length(6d).build();
+
+        Prop prop1 = Prop.builder()
+                .prop_name("casa1")
+                .prop_district(district1)
+                .prop_rooms(Arrays.asList(room1, room2)).build();
+
+        List<Prop> p = new ArrayList<Prop>();
+        p.add(prop1);
+        dto.add(ComodoDTO.converte(room1));
+        dto.add(ComodoDTO.converte(room2));
+        ImobiliariaRepository mock = Mockito.mock(ImobiliariaRepository.class);
+        Mockito.when(mock.getPropertiesList()).thenReturn(p);
+        ImobiliariaService propService = new ImobiliariaService(mock);
+
+        //act
+        List<ComodoDTO> listComodos = propService.areaComodos(prop1.getProp_name());
+
+
+
+        //assert
+        for (int i=0; i < listComodos.size(); i+=1) {
+            Double area = prop1.getProp_rooms().get(i).getRoom_length() * prop1.getProp_rooms().get(i).getRoom_width();
+            assertEquals(listComodos.get(i).getArea(), area);
+        }
+
+    }
+
 }
